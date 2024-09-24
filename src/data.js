c618db11a52fd3f6944bc23a9b4ea7095c1a8356
Freebo3110 <kutkova88@gmail.com>
2024-09-24 20:29:04 +0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const data = [
+
+    {
+    id: 1,
+    book: "S. King It",
+    image: "https://bookling.ua/upload/iblock/527/king_s_it_film_tie_in.jpeg"
+    },
+
+    { 
+    id: 2,
+    book: "J.K.Rowling Harry Potter",
+    image: "https://static.yakaboo.ua/media/catalog/product/8/1/816m1u8u4ql_cr.jpg"
+    },
+
+    {
+    id: 3,
+    book: "J.R.R. Tolkien The Lord of the Rings",
+    image: "https://www.britishbook.ua/upload/resize_cache/iblock/336/vcc9n87wof7fig06t1dgroqt6v3j4tcf/1900_800_174b5ed2089e1946312e2a80dcd26f146/kniga_the_lord_of_the_rings.jpeg"
+    }
+]
