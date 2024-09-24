771ca47c7f5961e38bbc976fde3894b1dfbf692b
Freebo3110 <kutkova88@gmail.com>
2024-09-24 10:28:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import { useState } from "react";
+
 
 function App() {
+  const [books, setBooks] = useState(data);
+
+  const removeBook = (id) =>{
+    let newBooks = books.filter(book => book.id !== id)
+    setBooks(newBooks)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>List of {books.length} books for reading</h1>
+      </div>
+      {books.map((element => {
+        const{id,book,image}=element;
+        return(<div key = "id">
+          <div className='container'>
+            <h2>{id} - {book}</h2>
+          </div>
+          <div className='container'>
+            <img src={image} width="300px" alt ="gift"/>
+          </div>
+          <div className='container'>
+              <button onClick = {()=> removeBook(id)}>Remove</button>
+          </div>
+        </div>)
+      }))}
+      <div className='container'>
+        <button onClick = {()=> setBooks([])}>delete all</button>
+      </div>
     </div>
   );
 }
